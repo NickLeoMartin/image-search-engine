c5afea828fe1c25729a3d7b5427caede63a494c9
Nicholas Leo Martin <nickleomartin@gmail.com>
2018-05-12 21:39:06 +0200

    Foundation for V1 complete

---
+++ match/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from picklefield.fields import PickledObjectField
+import datetime
+import json
+
+
+IMAGECHOICES = (('dataset', 'dataset'),('uploaded','uploaded'))
+
+class Image(models.Model):
+    """ Model to store path to images and features """
+    img_id = models.IntegerField(default=-1)
+    img_file = models.ImageField(upload_to=None)
+    img_features = PickledObjectField(default=0)
+    img_type = models.CharField(max_length=100,default='dataset',choices=IMAGECHOICES)
+
+    def __str__(self):
+        return "%s - %s"%(self.img_type,self.img_file.name)
+
+
+class MostSimilarImages(models.Model):
+	""" Storage for most similar images list of pks """
+    sim_uploaded_image = models.ForeignKey(UploadedImage,related_name='uploaded_image_most_sim',on_delete=models.CASCADE,null=True,blank=True)
+    sim_top_n = PickledObjectField(default='') ##list of ordered pks
+    sim_algo = models.CharField(max_length=100,default='')
+
+    def __str__(self):
+        return "%s - %s"%(self.sim_algo,self.sim_uploaded_image)
---
+++ read_in_dataset.py
@@ -0,0 +1,36 @@
+import glob
+import cv2
+
+from ml_models.color_descriptor import *
+from match.models import *
+
+## Change to read in from config file
+dataset_path = '/dataset/'
+
+## Initialise ColorDescriptor
+cd = ColorDescriptor((8, 12, 3))
+
+## Loop through images, obtain features and store in DB
+for imagePath in glob.glob(dataset_path + "/*.jpg"):
+        ## Extract the image ID (i.e. the unique filename) from the image
+        ## path and load the image itself
+        imageID = imagePath[imagePath.rfind("/") + 1:]
+        print('Image ID: ', imageID)
+
+        img_id = int(imageID.replace(".jpg",''))
+        
+        image = cv2.imread(imagePath)
+     
+        ## Describe the image
+        features = cd.describe(image)
+        
+        ## Save the features to db
+        __image, created = Image.objects.update_or_create(
+                                                        img_id=img_id,
+                                                        defaults = {
+                                                        "img_file": imagePath,
+                                                        "img_features": features,
+                                                        "img_type": "dataset",
+                                                        },)
+        if created:
+            print(created)
